e12fc844deab7ab0fd6b4c370b55cfbccd745169
rataprogramando <108586188+rataprogramando@users.noreply.github.com>
2022-08-10 20:14:24 -0300

    ejercicios realizados

--- homework.js
+++ homework.js
@@ -1,288 +1,104 @@
 // No cambies los nombres de las funciones.
 
-function devolverPrimerElemento(array) {
-  // Devuelve el primer elemento de un  array (pasado por parametro)
+function crearUsuario() {
+  // Crea una Clase de ES6 o una función constructor llamada "Usuario"
+  // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
+  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // {{nombre}} debe ser el nombre definido en cada instancia
+  // Devuelve la clase
   // Tu código:
-  return array[0]
-}
-
-
-function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
-  // Tu código:
-  return array[array.length-1]
-}
-
-
-function obtenerLargoDelArray(array) {
-  // Devuelve el largo de un array
-  // Tu código:
-  return array.length
-}
-
-
-function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
-  var  ai=[];
-  for (let index = 0; index < array.length; index++) {
-    ai.push(array[index]+1);
-   }
-  return ai
-}
-
-
-function agregarItemAlFinalDelArray(array, elemento) {
-  // Añade el "elemento" al final del array
-  // y devuelve el array
-  // Tu código:
-  array.push(elemento)
-  return array
-}
-
-
-function agregarItemAlComienzoDelArray(array, elemento) {
-  // Añade el "elemento" al comienzo del array
-  // y devuelve el array
-  // Pista: usa el método `.unshift`
-  // Tu código:
-  array.unshift(elemento)
-  return array
-}
-
-
-function dePalabrasAFrase(palabras) {
-  // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
-  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
-  return palabras.join(' ')
-}
-
-
-function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
-  // Tu código:
-  for (let index = 0; index < array.length; index++) {
-    if ( array[index]===elemento) {
-     return true
-    } 
-   } return false
-}
-
-
-function agregarNumeros(numeros) {
-  // "numeros" debe ser un arreglo de enteros (int/integers)
-  // Suma todos los enteros y devuelve el valor
-  // Tu código:
-  var suma=0
-  for (let index = 0; index < numeros.length; index++) {
-    suma = suma + numeros[index];
+  function Usuario(opciones){
+    this.usuario=opciones.usuario;
+    this.nombre=opciones.nombre;
+    this.email=opciones.email;
+    this.password=opciones.password;
   }
-  return suma
+  Usuario.prototype.saludar=function () {
+    return 'Hola, mi nombre es '+this.nombre;
+  }
+  return Usuario
 }
 
-
-function promedioResultadosTest(resultadosTest) {
-  // "resultadosTest" debe ser una matriz de enteros (int/integers)
-  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+function agregarMetodoPrototype(Constructor) {
+  // Agrega un método al Constructor del `prototype`
+  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  var promedio=0
-for (let index = 0; index < resultadosTest.length; index++) {
-  promedio += resultadosTest[index];
-}
-return promedio/resultadosTest.length
+  Constructor.prototype.saludar=function(){
+    return "Hello World!";
+  }
 }
 
-
-function numeroMasGrande(numeros) {
-  // "numeros" debe ser una matriz de enteros (int/integers)
-  // Devuelve el número más grande
-  // Tu código:
-  return Math.max.apply(null, numeros)
-}
-
-
-function multiplicarArgumentos() {
-  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
-  // Escribe tu código aquí:
-  var multiplicar=1; 
-  if (arguments.length===0) {
-    return 0
-  }else if (arguments.length==1) {
-    return arguments[0]
-  } else {
-    for (let index = 0; index < arguments.length; index++) {
-      multiplicar = multiplicar * arguments[index];
+function agregarStringInvertida() {
+  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+  // El método debe llamarse "reverse"
+  // Ej: 'menem'.reverse() => menem
+  // 'toni'.reverse() => 'inot'
+  // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse=function(){
+    var strIn=""
+    for (let index = this.length-1; index>=0; index--) {
+      strIn = strIn + this[index];
+      
     }
-    return multiplicar
+    return strIn
   }
 }
 
+// ---------------------------------------------------------------------------//
+  //Crea el constructor de la clase "Persona"
+  //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
+  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+  //Ej: { 
+    //   Nombre: 'Juan',
+    //   Apellido: 'Perez',
+    //   Edad: 22,
+    //   Domicilio: 'Saavedra 123'
+    //  }
 
-function cuentoElementos(arreglo){
-  //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
-  //Escribe tu código aquí
-  var contador=0;
-  for (let index = 0; index < arreglo.length; index++) {
-    if (arreglo[index]>18) {
-      contador++
-    } 
-  }
-  return contador
-}
-
-
-function diaDeLaSemana(numeroDeDia) {
-  //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
-  //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
-  //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  if (numeroDeDia===1||numeroDeDia===7) {
-    return 'Es fin de semana'
-  } else if(numeroDeDia>1&&numeroDeDia<7){
-    return 'Es dia Laboral'
-  }
-} 
-
-
-function empiezaConNueve(n) {
-  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
-  //inicia con 9 y false en otro caso.
-  //Escribe tu código aquí
-  var ns=n.toString()
-  if (ns[0]==='9') {
-    return true
-  } else {
-   return false
-  }
-}
-
-
-function todosIguales(arreglo) {
-  //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
-  //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  let contador=0
-  for (let index = 0; index < arreglo.length-1; index++) {
-    if (arreglo[index] === arreglo[index+1]){
-      contador ++
-    } 
-  }
-  if(contador===arreglo.length-1){
-    return true
-  } else{return false}
-} 
-
-
-function mesesDelAño(array) {
-  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
-  // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
-  //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
-  var meses=[];
-  for (let index = 0; index < array.length; index++) {
-    if(array[index]=== "Enero" || array[index]=== "Marzo" || array[index]=== "Noviembre" ){
-    meses.push(array[index])
-  }} 
-  if (meses.length===3) {
-    return meses
-  }
-  else{
-    return "No se encontraron los meses pedidos"
+  class Persona {
+    constructor(nombre,apellido,edad,domicilio/*Escribir los argumentos que recibe el constructor*/) {
+      // Crea el constructor:
+      this.nombre=nombre
+      this.apellido=apellido
+      this.edad=edad
+      this.domicilio=domicilio
+      this.detalle = function(){
+        return {
+          Nombre:this.nombre,
+          Apellido:this.apellido,
+          Edad:this.edad,
+          Domicilio:this.domicilio,
+        }
       }
-}
- 
-
-function mayorACien(array) {
-  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
-  //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
-  // Tu código:
-  var mayoresACien=[]
-  for (let index = 0; index < array.length; index++) {
-    if (array[index]>100) {
-      mayoresACien.push(array[index])
     }
-  }
-  return mayoresACien
 }
 
-
-function breakStatement(numero) {
-  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
-  //Guardar cada nuevo valor en un array. 
-  //Devolver el array
-  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
-  //devolver: "Se interrumpió la ejecución"
-  //Pista: usá el statement 'break'
-  // Tu código:
-  var nuevo=[]
-  var suma = numero
-for (let index = 0; index < 10; index++) {
-  suma = suma + 2
-  if (suma===index) {
-    break;
-  } else {
-    nuevo.push(suma)
+function crearInstanciaPersona(nombre, apellido, edad, dir) {
+  //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
+  //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
+  //Devolver la nueva persona creada
+  var persona= new Persona(nombre, apellido, edad, dir)
+  return persona
+}
+  
+function agregarMetodo() {
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
+  //Ej: "Juan, 22 años"
+  Persona.prototype.datos=function () {
+    return (this.nombre+", "+this.edad+" años")
   }
 }
-if (nuevo.length<10) {
-  return "Se interrumpió la ejecución"
-} else {
-  return nuevo
-}
-}
-
-
-
-function continueStatement(numero) {
-  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
-  //Guardar cada nuevo valor en un array.    
-  //Devolver el array
-  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
-  //Pista: usá el statement 'continue'
-  // Tu código:
-  var nuevo=[]
-  var suma = numero
-for (let index = 0; index < 10; index++) {
-  if (index===5) {
-    continue
-  } else {
-    suma = suma + 2
-    nuevo.push(suma)
-  }
-}
-  return nuevo
-}
-
+  
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  devolverPrimerElemento,
-  devolverUltimoElemento,
-  obtenerLargoDelArray,
-  incrementarPorUno,
-  agregarItemAlFinalDelArray,
-  agregarItemAlComienzoDelArray,
-  dePalabrasAFrase,
-  arrayContiene,
-  agregarNumeros,
-  promedioResultadosTest,
-  numeroMasGrande,
-  multiplicarArgumentos,
-  cuentoElementos,
-  diaDeLaSemana,
-  empiezaConNueve,
-  todosIguales,
-  mesesDelAño,
-  mayorACien,
-  breakStatement,
-  continueStatement
+  crearUsuario,
+  agregarMetodoPrototype,
+  agregarStringInvertida,
+  crearInstanciaPersona,
+  agregarMetodo, 
+  Persona
 };
